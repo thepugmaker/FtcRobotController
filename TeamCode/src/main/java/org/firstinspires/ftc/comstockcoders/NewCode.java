8cec8393c70fe9aec32d887f887f0d146fd4ef3a
vcmsrobot <contact@mrpugpug.com>
2025-02-07 17:17:30 -0800

    Forgot a line

--- TeamCode/src/main/java/org/firstinspires/ftc/comstockcoders/NewCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/comstockcoders/NewCode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.comstockcoders;
 
 import androidx.appcompat.app.WindowDecorActionBar;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,7 +11,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "NewCode")
-
+@Disabled
 public class NewCode extends LinearOpMode {
 
     private DcMotor Arm_Motor;
